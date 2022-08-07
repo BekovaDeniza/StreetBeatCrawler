6e4fd8303b78d5dff29801a80331e76509170189
BekovaDeniza <bekova_02@inbox.ru>
2022-08-07 17:42:32 +0300

    fix parser and requirements

--- crawler.py
+++ crawler.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 
@@ -10,49 +10,48 @@ from datetime import datetime
 from time import sleep, time
 import json
 import re
+import os
 
 start_time = time()
 
 
 class StreetBeat:
-    url = 'https://street-beat.ru/cat/kids/?page=%s'
+    url = 'https://street-beat.ru/cat/?page=%s'
     product_list = []
 
     def __init__(self):
         options = webdriver.ChromeOptions()
+        options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64)AppleWebKit/537.36 (KHTML, '
+                             'like Gecko)Chrome/99.0.4844.51 Safari/537.36')
         options.add_argument("start-maximized")
         options.add_argument('--disable-blink-features=AutomationControlled')
+
         options.add_experimental_option("excludeSwitches", ['enable-automation', 'enable-logging'])
         options.add_experimental_option('useAutomationExtension', False)
         options.add_argument('--headless')
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
         self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-        self.driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument",
-                                    {"source": "const newProto = navigator.__proto__;"
-                                               "delete newProto.webdriver;"
-                                               "navigator.__proto__ = newProto;"})
-        self.driver.execute_cdp_cmd('Network.setUserAgentOverride',
-                                    {"userAgent": 'Mozilla/5.0 (X11; Linux x86_64) '
-                                                  'AppleWebKit/537.36 (KHTML, like Gecko) '
-                                                  'Chrome/99.0.4844.51 Safari/537.36'})
         self.wait = WebDriverWait(self.driver, 10)
 
     def bypass_all_pages(self):
         """
         We find out the number of pages in the catalog and ask each.
         """
-        link = 'https://street-beat.ru/cat/kids/'
+        link = 'https://street-beat.ru/cat'
+        date_list = [
+            self.get_added() if os.path.exists('street-beat.json') else datetime.now().strftime("%Y-%m-%d %H:%M"),
+            datetime.now().strftime("%Y-%m-%d %H:%M")]
         data = self.get_json_data(link)
         pages_count = data['catalog']['pagination']['lastPage']
         for i in range(1, pages_count + 1):
             print(f'parsing {i} page out of {pages_count}')
-            self.parse_items(self.url % i)
-            self.driver.quit()
+            self.parse_items(self.url % i, date_list)
 
-    def parse_items(self, link):
+    def parse_items(self, link, date_list):
         """
         We parse all links on the page.
         We parse the data that is in the api, the rest from the product card.
+        :param date_list: list of date and time of adding and updating parsing.
         :param link: link to current page.
         """
         data = self.get_json_data(link)
@@ -63,7 +62,7 @@ class StreetBeat:
             self.driver.get(url)
             sleep(3)
             self.wait.until(
-               EC.presence_of_element_located((By.XPATH, '/html/body/article/div[1]/div')))
+                EC.presence_of_element_located((By.XPATH, '/html/body/article/div[1]/div')))
             soup = BeautifulSoup(self.driver.page_source, 'lxml')
             category = soup.find_all('a', {'class': 'breadcrumbs__link'})
             category1 = category[-4].text.strip()
@@ -71,35 +70,41 @@ class StreetBeat:
             category3 = category[-2].text.strip()
             category4 = category[-1].text.strip()
             try:
-                brand = soup.find("div", {"class": "tags-list"}).find_all('a')[2].text.replace("Другие товары", "").strip()
+                brand = soup.find("div", {"class": "tags-list"}).find_all('a')[2].text.replace("Другие товары",
+                                                                                               "").strip()
             except:
                 brand = ''
             try:
                 description = soup.find('div', {'class': 'tab__header'}).find('div', {'class': 'tab__description'}).text
             except:
                 description = ''
-            added = datetime.now().strftime("%Y-%m-%d %H:%M")
             updated = datetime.now().strftime("%Y-%m-%d %H:%M")
             self.product_list.append({
                 'SKU': item['id'], 'Vendor code': '', 'Name': item['title'], 'Brand': brand,
                 'Category1': category1, 'Category2': category2, 'Category3': category3, 'Category4': category4,
                 'Price old': item['price']['recommended']['price'],
                 'Price': item['price']['special']['price'],
-                'Price unit': item['price']['recommended']['currency'], 'Available': 'True', 'Added': added,
-                'Updated': updated, 'Url': url, 'Image url': item['image']['main']['desktopX2'],
+                'Price unit': item['price']['recommended']['currency'], 'Available': 'True',
+                'Added': date_list[0],
+                'Updated': date_list[1], 'Url': url, 'Image url': item['image']['main']['desktopX2'],
                 'txtDescription': description
             })
 
     def get_json_data(self, link):
         """
         :param link: link to current page.
-        :return: api data from the page in .json format.
+        :return: api data from the page in json format.
         """
         self.driver.get(link)
         self.wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div')))
         data = re.search(r'__INITIAL_STATE__ \= JSON\.parse\(\'(.+)\'\);\n', self.driver.page_source).group(
             1).replace('\\', '')
         return json.loads(data)
+
+    def get_added(self):
+        with open('street-beat.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            return data[0]['Added']
 
     def save_data(self):
         """
